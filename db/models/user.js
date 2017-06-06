fc87abfc9e8fe45f5d2fbf83e4374149c1df1684
Jin Lin <jinlin314@hotmail.com>
2017-06-06 12:53:06 -0400

    done with user model

--- db/models/user.js
+++ db/models/user.js
@@ -5,75 +5,73 @@
 const bcrypt = require('bcryptjs')
     , {STRING, VIRTUAL, BOOLEAN} = require('sequelize')
 
-module.exports = db => {
-  return db.define('users', {
-    name: STRING,
-    email: {
-      type: STRING,
-      validate: {
-        isEmail: true,
-        notEmpty: true,
-      }
-    },
+module.exports = db => db.define('users', {
+  name: STRING,
+  email: {
+    type: STRING,
+    validate: {
+      isEmail: true,
+      notEmpty: true,
+    }
+  },
 
     // We support oauth, so users may or may not have passwords.
-    password_digest: STRING, // This column stores the hashed password in the DB, via the beforeCreate/beforeUpdate hooks
-    password: VIRTUAL, // Note that this is a virtual, and not actually stored in DB
-    isAdmin: BOOLEAN,
-    facebookLink: {
-      type: STRING,
-      validate: {
-        isUrl: true
-      }
-    },
-    twitterLink: {
-      type: STRING,
-      validate: {
-        isUrl: true
-      }
-    },
-    instagramLink: {
-      type: STRING,
-      validate: {
-        isUrl: true
-      }
-    },
-    snapChatLink: {
-      type: STRING,
-      validate: {
-        isUrl: true
-      }
-    },
-    address: STRING,
-    paypalId: {
-      type: STRING,
-      validate: {
-        isEmail: true
-      }
-    },
-    amazonPayId: {
-      type: STRING,
-      validate: {
-        isEmail: true
-      }
+  password_digest: STRING, // This column stores the hashed password in the DB, via the beforeCreate/beforeUpdate hooks
+  password: VIRTUAL, // Note that this is a virtual, and not actually stored in DB
+  isAdmin: BOOLEAN,
+  facebookLink: {
+    type: STRING,
+    validate: {
+      isUrl: true
     }
-  }, {
-    indexes: [{fields: ['email'], unique: true}],
-    hooks: {
-      beforeCreate: setEmailAndPassword,
-      beforeUpdate: setEmailAndPassword,
-    },
-    defaultScope: {
-      attributes: {exclude: ['password_digest']}
-    },
-    instanceMethods: {
+  },
+  twitterLink: {
+    type: STRING,
+    validate: {
+      isUrl: true
+    }
+  },
+  instagramLink: {
+    type: STRING,
+    validate: {
+      isUrl: true
+    }
+  },
+  snapChatLink: {
+    type: STRING,
+    validate: {
+      isUrl: true
+    }
+  },
+  address: STRING,
+  paypalId: {
+    type: STRING,
+    validate: {
+      isEmail: true
+    }
+  },
+  amazonPayId: {
+    type: STRING,
+    validate: {
+      isEmail: true
+    }
+  }
+}, {
+  indexes: [{fields: ['email'], unique: true}],
+  hooks: {
+    beforeCreate: setEmailAndPassword,
+    beforeUpdate: setEmailAndPassword,
+  },
+  defaultScope: {
+    attributes: {exclude: ['password_digest']}
+  },
+  instanceMethods: {
       // This method is a Promisified bcrypt.compare
-      authenticate(plaintext) {
-        return bcrypt.compare(plaintext, this.password_digest)
-      }
+    authenticate(plaintext) {
+      return bcrypt.compare(plaintext, this.password_digest)
     }
-  })
-}
+  }
+})
 
 module.exports.associations = (User, {OAuth, Thing, Favorite}) => {
   User.hasOne(OAuth)
