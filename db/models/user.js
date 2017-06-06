f4ffdd342b601aaa9f8d2473a455851d2847b1ea
Jin Lin <jinlin314@hotmail.com>
2017-06-06 16:05:19 -0400

    User model #4 (#16)
    * created a user model branch
    
    * created a user model branch
    
    * done with user model
    
    * done with user model

--- db/models/user.js
+++ db/models/user.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-trailing-spaces */
 'use strict'
 
 // bcrypt docs: https://www.npmjs.com/package/bcrypt
 const bcrypt = require('bcryptjs')
-    , {STRING, VIRTUAL} = require('sequelize')
+    , {STRING, VIRTUAL, BOOLEAN} = require('sequelize')
 
 module.exports = db => db.define('users', {
   name: STRING,
@@ -14,9 +15,47 @@ module.exports = db => db.define('users', {
     }
   },
 
-  // We support oauth, so users may or may not have passwords.
+    // We support oauth, so users may or may not have passwords.
   password_digest: STRING, // This column stores the hashed password in the DB, via the beforeCreate/beforeUpdate hooks
-  password: VIRTUAL // Note that this is a virtual, and not actually stored in DB
+  password: VIRTUAL, // Note that this is a virtual, and not actually stored in DB
+  isAdmin: BOOLEAN,
+  facebookLink: {
+    type: STRING,
+    validate: {
+      isUrl: true
+    }
+  },
+  twitterLink: {
+    type: STRING,
+    validate: {
+      isUrl: true
+    }
+  },
+  instagramLink: {
+    type: STRING,
+    validate: {
+      isUrl: true
+    }
+  },
+  snapChatLink: {
+    type: STRING,
+    validate: {
+      isUrl: true
+    }
+  },
+  address: STRING,
+  paypalId: {
+    type: STRING,
+    validate: {
+      isEmail: true
+    }
+  },
+  amazonPayId: {
+    type: STRING,
+    validate: {
+      isEmail: true
+    }
+  }
 }, {
   indexes: [{fields: ['email'], unique: true}],
   hooks: {
@@ -27,7 +66,7 @@ module.exports = db => db.define('users', {
     attributes: {exclude: ['password_digest']}
   },
   instanceMethods: {
-    // This method is a Promisified bcrypt.compare
+      // This method is a Promisified bcrypt.compare
     authenticate(plaintext) {
       return bcrypt.compare(plaintext, this.password_digest)
     }
