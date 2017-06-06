30f67e089ab83f99721dd89b6ec946447c7aabc1
Allie Fauer <alliefauer@gmail.com>
2017-06-06 17:01:02 -0400

    edited prepush

--- db/models/index.js
+++ db/models/index.js
@@ -3,7 +3,7 @@
 // Require our models. Running each module registers the model into sequelize
 // so any other part of the application could call sequelize.model('User')
 // to get access to the User model.
-
+const git = 'workplease'
 const app = require('APP')
     , debug = require('debug')(`${app.name}:models`)
     // Our model files export functions that take a database and return
