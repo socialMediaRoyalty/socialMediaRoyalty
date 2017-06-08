eb33a08904eeb711db140913c56b735e179fb294
Allie Fauer <alliefauer@gmail.com>
2017-06-08 12:27:51 -0400

    edited routes and admin privileges (#33)

--- server/users.js
+++ server/users.js
@@ -3,17 +3,11 @@
 const db = require('APP/db')
 const User = db.model('users')
 
-const {mustBeLoggedIn, forbidden} = require('./auth.filters')
+const {mustBeLoggedIn, assertAdmin, selfOnly} = require('./auth.filters')
 
 module.exports = require('express').Router()
   .get('/',
-    // The forbidden middleware will fail *all* requests to list users.
-    // Remove it if you want to allow anyone to list all users on the site.
-    //
-    // If you want to only let admins list all the users, then you'll
-    // have to add a role column to the users table to support
-    // the concept of admin users.
-    // forbidden('listing users is not allowed'),
+    assertAdmin,
     (req, res, next) =>
       User.findAll()
         .then(users => res.json(users))
@@ -24,8 +18,41 @@ module.exports = require('express').Router()
       .then(user => res.status(201).json(user))
       .catch(next))
   .get('/:id',
-    mustBeLoggedIn,
+    selfOnly,
     (req, res, next) =>
       User.findById(req.params.id)
       .then(user => res.json(user))
+      .catch(next))
+  .put('/:id',
+      assertAdmin,
+      (req, res, next) =>
+        User.findById(req.params.id)
+        .then(user => user.update({
+          isAdmin: req.body.isAdmin,
+        }))
+        .then(updatedUser => res.json(updatedUser))
+        .catch(next))
+  .put('/:id',
+    selfOnly,
+    (req, res, next) =>
+      User.findById(req.params.id)
+      .then(user => user.update({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        twitterLink: req.body.twitterLink,
+        instagramLink: req.body.instagramLink,
+        snapChatLink: req.body.snapChatLink,
+        address: req.body.address,
+        paypalId: req.body.paypalId,
+        amazonPayId: req.body.amazonPayId
+      }))
+      .then(updatedUser => res.json(updatedUser))
+      .catch(next))
+  .delete('/:id',
+    assertAdmin,
+    (req, res, next) =>
+      User.findById(req.params.id)
+      .then(user => user.destroy())
+      .then(() => res.status(204).end())
       .catch(next))
