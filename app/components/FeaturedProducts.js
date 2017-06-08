74b47e238c675c2a8aeaf385170c7283ad9ff08a
Jin Lin <jinlin314@hotmail.com>
2017-06-08 18:05:35 -0400

    fixed all issues kate commented (#44)

---
+++ app/components/FeaturedProducts.js
@@ -0,0 +1,257 @@
+import React from 'react'
+
+export const FeaturedProducts = (props) => {
+  return (
+    <div className="span9">
+      <div className="well well-small">
+        <h4>Featured Products <small className="pull-right">200+ featured products</small></h4>
+        <div className="row-fluid">
+          <div id="featured" className="carousel slide">
+            <div className="carousel-inner">
+              <div className="item active">
+                <ul className="thumbnails">
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div className="item">
+                <ul className="thumbnails">
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div className="item">
+                <ul className="thumbnails">
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div className="item">
+                <ul className="thumbnails">
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="span3">
+                    <div className="thumbnail">
+                      <a href="product_details.html"><img src="themes/images/products/default.jpg" alt="" /></a>
+                      <div className="caption">
+                        <h5>Product name</h5>
+                        <h4><a className="btn" href="product_details.html">VIEW</a> <span className="pull-right">$222.00</span></h4>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <a className="left carousel-control" href="#featured" data-slide="prev">‹</a>
+            <a className="right carousel-control" href="#featured" data-slide="next">›</a>
+          </div>
+        </div>
+      </div>
+      <h4>Latest Products </h4>
+      <ul className="thumbnails">
+        <li className="span3">
+          <div className="thumbnail">
+            <a href="product_details.html"><img src="themes/images/products/default.jpg" alt=""/></a>
+            <div className="caption">
+              <h5>Product name</h5>
+              <p>
+                Lorem Ipsum is simply dummy text.
+              </p>
+
+              <h4 style="text-align:center"><a className="btn" href="product_details.html"> <i className="icon-zoom-in">+</i></a> <a className="btn" href="#">Add to <i className="icon-shopping-cart">+</i></a> <a className="btn btn-primary" href="#">$222.00</a></h4>
+            </div>
+          </div>
+        </li>
+        <li className="span3">
+          <div className="thumbnail">
+            <a href="product_details.html"><img src="themes/images/products/default.jpg" alt=""/></a>
+            <div className="caption">
+              <h5>Product name</h5>
+              <p>
+                Lorem Ipsum is simply dummy text.
+              </p>
+              <h4 style="text-align:center"><a className="btn" href="product_details.html"> <i className="icon-zoom-in">+</i></a> <a className="btn" href="#">Add to <i className="icon-shopping-cart">+</i></a> <a className="btn btn-primary" href="#">$222.00</a></h4>
+            </div>
+          </div>
+        </li>
+        <li className="span3">
+          <div className="thumbnail">
+            <a href="product_details.html"><img src="themes/images/products/default.jpg" alt=""/></a>
+            <div className="caption">
+              <h5>Product name</h5>
+              <p>
+                Lorem Ipsum is simply dummy text.
+              </p>
+              <h4 style="text-align:center"><a className="btn" href="product_details.html"> <i className="icon-zoom-in">+</i></a> <a className="btn" href="#">Add to <i className="icon-shopping-cart">+</i></a> <a className="btn btn-primary" href="#">$222.00</a></h4>
+            </div>
+          </div>
+        </li>
+        <li className="span3">
+          <div className="thumbnail">
+            <a href="product_details.html"><img src="themes/images/products/default.jpg" alt=""/></a>
+            <div className="caption">
+              <h5>Product name</h5>
+              <p>
+                Lorem Ipsum is simply dummy text.
+              </p>
+              <h4 style="text-align:center"><a className="btn" href="product_details.html"> <i className="icon-zoom-in">+</i></a> <a className="btn" href="#">Add to <i className="icon-shopping-cart">+</i></a> <a className="btn btn-primary" href="#">$222.00</a></h4>
+            </div>
+          </div>
+        </li>
+        <li className="span3">
+          <div className="thumbnail">
+            <a href="product_details.html"><img src="themes/images/products/default.jpg" alt=""/></a>
+            <div className="caption">
+              <h5>Product name</h5>
+              <p>
+                Lorem Ipsum is simply dummy text.
+              </p>
+              <h4 style="text-align:center"><a className="btn" href="product_details.html"> <i className="icon-zoom-in">+</i></a> <a className="btn" href="#">Add to <i className="icon-shopping-cart">+</i></a> <a className="btn btn-primary" href="#">$222.00</a></h4>
+            </div>
+          </div>
+        </li>
+        <li className="span3">
+          <div className="thumbnail">
+            <a href="product_details.html"><img src="themes/images/products/default.jpg" alt=""/></a>
+            <div className="caption">
+              <h5>Product name</h5>
+              <p>
+                Lorem Ipsum is simply dummy text.
+              </p>
+              <h4 style="text-align:center"><a className="btn" href="product_details.html"> <i className="icon-zoom-in">+</i></a> <a className="btn" href="#">Add to <i className="icon-shopping-cart">+</i></a> <a className="btn btn-primary" href="#">$222.00</a></h4>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default FeaturedProducts
