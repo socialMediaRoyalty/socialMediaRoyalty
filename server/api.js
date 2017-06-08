dd5979fabc6d046acefcdd06ebf2e89aecb423d7
Stella <stll.chung@gmail.com>
2017-06-08 12:31:21 -0400

    Cart routes #23 (#35)
    * cart route, no tests working
    
    * in-progress, cart routes

--- server/api.js
+++ server/api.js
@@ -6,6 +6,7 @@ api
   .get('/heartbeat', (req, res) => res.send({ok: true}))
   .use('/auth', require('./auth'))
   .use('/users', require('./users'))
+  .use('/cart', require('./cart'))
   .use('/products', require('./product'))
   .use('/categories', require('./category'))
 
