ee5db7f5a784b92f4926b8e49884735719e0bc56
Allie Fauer <alliefauer@gmail.com>
2017-06-09 12:55:15 -0400

    all user routes working

--- server/auth.filters.js
+++ server/auth.filters.js
@@ -1,13 +1,13 @@
 const mustBeLoggedIn = (req, res, next) => {
   if (!req.user) {
-    return res.status(401).send('You must be logged in')
+    return res.status(401).json('You must be logged in')
   }
   next()
 }
 
 const selfOnly = action => (req, res, next) => {
-  if (req.params.id !== req.user.id) {
-    return res.status(403).send(`You can only ${action} yourself.`)
+  if (req.params.uid !== req.user.id) {
+    return res.status(403).json(`You can only ${action} yourself.`)
   }
   next()
 }
@@ -15,14 +15,24 @@ const selfOnly = action => (req, res, next) => {
 // const forbidden = message => (req, res) => {
 //   res.status(403).send(message)
 // }
+
+const assertSelfOrAdmin = (req, res, next) => {
+  if(!req.user) {
+    return res.status(401).json('You must be logged in')
+  } else if (!req.user.isAdmin && req.requestedUser.dataValues.id !== req.user.dataValues.id) {
+    return res.status(403).json('You are unauthorized to view this page test')
+  }
+  next()
+}
+
 const assertAdmin = (req, res, next) => {
   if (!req.user) {
-    return res.status(401).send('You must be logged in');
+    return res.status(401).json('You must be logged in')
   } else if (!req.user.isAdmin) {
-    return res.status(403).send('You are unauthorized to view this page')
+    return res.status(403).json('You are unauthorized to view this page')
   } 
-  next();
+  next()
 }
 
 // Feel free to add more filters here (suggested: something that keeps out non-admins)
-module.exports = {mustBeLoggedIn, selfOnly, assertAdmin}
+module.exports = {mustBeLoggedIn, selfOnly, assertAdmin, assertSelfOrAdmin}
