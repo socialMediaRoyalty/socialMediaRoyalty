385911f1dcb8e95e6f3eae7be13c4b2df756168b
Allie Fauer <alliefauer@gmail.com>
2017-06-09 14:45:55 -0400

    all user routes working

--- server/api.js
+++ server/api.js
@@ -10,7 +10,5 @@ api
   .use('/cart', require('./cart'))
   .use('/products', require('./product'))
   .use('/categories', require('./category'))
-
-
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
