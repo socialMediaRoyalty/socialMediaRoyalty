93d725e7cc623a82a4b3a55df5b35dd6e9159f65
Stella Chung <stll.chung@gmail.com>
2017-06-10 16:55:29 -0400

    setup root-component and blank files for Navbar and Footer

---
+++ app/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Home = () => (
+  <div>
+    <h1>"Hey I'm the Home Page"</h1>
+    <h3>Probably see a lot of stuff here</h3>
+  </div>
+)
+
+export default Home
