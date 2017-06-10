225296ea1bf279cfa455e1f03395ae87c466847c
Stella <stll.chung@gmail.com>
2017-06-10 16:57:09 -0400

    setup root-component and blank files for Navbar and Footer (#83)

--- app/main.jsx
+++ app/main.jsx
@@ -1,6 +1,6 @@
 'use strict'
 import React from 'react'
-import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
+import {Router, Route, IndexRedirect, IndexRoute, browserHistory} from 'react-router'
 import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 import { Button } from 'react-bootstrap'
@@ -10,13 +10,14 @@ import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 import NotFound from './components/NotFound'
+import Root from './components/Root'
+import Home from './components/Home'
 
 const ExampleApp = connect(
   ({ auth }) => ({ user: auth })
 )(
   ({ user, children }) =>
     <div>
-      <Button bsStyle="success">Hi I'm a Bootstrap Button!</Button>
       <nav>
         {user ? <WhoAmI/> : <Login/>}
       </nav>
@@ -27,8 +28,8 @@ const ExampleApp = connect(
 render(
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={ExampleApp}>
-        <IndexRedirect to="/jokes" />
+      <Route path="/" component={Root}>
+        <IndexRoute component={Home} />
         <Route path="/jokes" component={Jokes} />
       </Route>
       <Route path='*' component={NotFound} />
