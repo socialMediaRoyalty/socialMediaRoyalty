171c7f5dddcb4ead121af792828d77d64db32c78
Stella Chung <stll.chung@gmail.com>
2017-06-12 17:25:05 -0400

    #65 Working: Home, Admin/Users, Login

--- app/components/WhoAmI.jsx
+++ app/components/WhoAmI.jsx
@@ -5,7 +5,7 @@ export const WhoAmI = ({ user, logout }) => (
   <div className="whoami">
     <span className="whoami-user-name">{user && user.name}</span>
     {' '}
-    <Button className="logout" onClick={logout}>Logout</Button>
+    <button className="logout btn btn-default" onClick={logout}>Logout</button>
   </div>
 )
 
