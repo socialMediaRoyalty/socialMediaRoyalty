6bf7ce1e9e2b99a7ff60080f360ebf77a8a1afc5
Stella Chung <stll.chung@gmail.com>
2017-06-11 22:09:36 -0400

    started the navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 
-export const Navbar = ({ login }) => (
-  <div className="col-lg-12">
-    <h1>This is navbar component</h1>
-  </div>
+export const Navigation = ({ login }) => (
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">Home</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav>
+      <NavDropdown eventKey={1} title="Categories" id="basic-nav-dropdown">
+        <MenuItem eventKey={1.1}>Action</MenuItem>
+        <MenuItem eventKey={1.2}>Another action</MenuItem>
+        <MenuItem eventKey={1.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={1.4}>Separated link</MenuItem>
+      </NavDropdown>
+      <NavItem eventKey={2} href="#">Sign in</NavItem>
+      <NavItem eventKey={3} href="#">Cart</NavItem>
+    </Nav>
+  </Navbar>
 )
 
 import {login} from 'APP/app/reducers/auth'
@@ -12,4 +28,4 @@ import {connect} from 'react-redux'
 export default connect(
   state => ({}),
   {login},
-)(Navbar)
+)(Navigation)
--- app/containers/RootContainer.jsx
+++ app/containers/RootContainer.jsx
@@ -14,6 +14,7 @@ export default class RootContainer extends Component {
     return (
       <div className="container">
         <div className="row">
+          <h1>Social Media Royalty</h1>
           <Navbar />
         </div>
         <div className="row">
